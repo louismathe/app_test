d2a236464d38bf5d7df6cae0a680fa3405161d16
Louis Mathe <mathe.louis@gmail.com>
2018-01-29 17:49:48 +0100

    Add Create to RestaurantsController

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,8 +13,13 @@ class RestaurantsController < ApplicationController
   end
 
 
-  def created
-
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -35,7 +40,7 @@ class RestaurantsController < ApplicationController
   end
 
   def restaurant_params
-
+    params.require(:restaurant).permit(:name, :address, :description)
   end
 
 end
