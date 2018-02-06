914c64ac1d2d12f4319301944d8f37fc6ea9d6e5
Louis Mathe <mathe.louis@gmail.com>
2018-02-06 14:11:51 +0100

    Add Pages contact & about +  hide update/destoy in restaurant index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!
+  skip_after_action :verify_authorized
 
   def home
+  end
+
+  def contact
+  end
+
+  def about
   end
 end
