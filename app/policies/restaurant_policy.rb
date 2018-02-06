d9ba6c174a6e1b90a400fc4c18842cccd90f6a37
Louis Mathe <mathe.louis@gmail.com>
2018-02-06 14:33:48 +0100

    update admin rights

--- app/policies/restaurant_policy.rb
+++ app/policies/restaurant_policy.rb
@@ -10,20 +10,26 @@ class RestaurantPolicy < ApplicationPolicy
     true
   end
 
-  def update?
-    record.user == user
-  end
-
-  def destroy?
-    record.user == user
-  end
-
   def chef?
     true
   end
 
   def top?
     true
+  end
+
+  def update?
+    user_is_owner_or_admin?
+  end
+
+  def destroy?
+    user_is_owner_or_admin?
+  end
+
+  private
+
+  def user_is_owner_or_admin?
+    user.admin || record.user == user
   end
 
 end
