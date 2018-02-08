eb7313ab44f0ebdca6ce756b9ef3f6050a2e7de2
Louis Mathe <mathe.louis@gmail.com>
2018-02-08 16:43:18 +0100

    Resturant destroy api

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User, except: [ :index, :show ]
-  before_action :set_restaurant, only: [ :show, :update ]
+  before_action :set_restaurant, only: [ :show, :update, :destroy]
 
   def index
     @restaurants = policy_scope(Restaurant)
@@ -14,7 +14,7 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
     @restaurant.user = current_user
     authorize @restaurant
     if @restaurant.save
-      render :show
+      render :show, status: :created
     else
       render_error
     end
@@ -26,6 +26,11 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
    else
      render_error
    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    head :no_content
   end
 
   private
