76639378364a3a49c31961efc664c8321c486093
Louis Mathe <mathe.louis@gmail.com>
2018-02-08 16:16:20 +0100

    fix API restaurant update

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User, except: [ :index, :show ]
-  before_action :set_restaurant, only: [ :show ]
+  before_action :set_restaurant, only: [ :show, :update ]
 
   def index
     @restaurants = policy_scope(Restaurant)
